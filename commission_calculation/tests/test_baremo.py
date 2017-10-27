998ec1edcb97db832609c4621d06c4f3d98d6619
Yanina Aular <yanina.aular@vauxoo.com>
2017-10-27 17:59:41 +0000

    [ADD] add test to baremo and fix constrains

--- commission_calculation/tests/test_baremo.py
+++ commission_calculation/tests/test_baremo.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 
 from odoo.tests.common import TransactionCase
+from odoo.exceptions import ValidationError
+from psycopg2 import IntegrityError
+from odoo.tools.misc import mute_logger
 
 
 class TestBaremo(TransactionCase):
-
-    """Tests for Commissions (commission.payment)
-    """
 
     def setUp(self):
         """basic method to define some basic data to be re use in all test cases.
@@ -20,4 +20,42 @@ class TestBaremo(TransactionCase):
         company.baremo_id = baremo_id
         self.assertEquals(baremo_id, company.baremo_id)
         self.assertEquals(baremo_id, company.partner_id.baremo_id)
-        return True
+
+    def test_baremo_matrix(self):
+        baremo_id = self.env.ref('commission_calculation.baremo_book_01')
+        ipad = self.env.ref('product.product_product_4')
+        matrix_obj = self.env['baremo.matrix']
+
+        matrix_obj.create({
+            'baremo_id': baremo_id.id,
+            'user_id': False,
+            'product_id': ipad.id,
+        })
+
+        error = 'A line of %s already exists for all salesman.' % ipad.name
+        with self.assertRaisesRegexp(ValidationError, error):
+            matrix_obj.create({
+                'baremo_id': baremo_id.id,
+                'user_id': False,
+                'product_id': ipad.id,
+            })
+
+        with self.assertRaises(IntegrityError), mute_logger('odoo.sql_db'):
+            matrix_obj.create({
+                'baremo_id': baremo_id.id,
+                'user_id': self.ref('base.user_root'),
+                'product_id': ipad.id,
+            })
+
+    def test_baremo_action_view_matrix(self):
+        baremo_1 = self.env.ref('commission_calculation.baremo_book_01')
+        result = baremo_1.action_view_matrix()
+
+        self.assertEquals(result['domain'],
+                          [('baremo_id', 'in', [baremo_1.id])])
+
+        baremo_2 = self.env.ref('commission_calculation.baremo_book_02')
+        result = baremo_2.action_view_matrix()
+
+        self.assertEquals(result['domain'],
+                          [('baremo_id', 'in', [baremo_2.id])])
