f03504ae64617d50fb6601edfdb44e32fa639419
Humberto Arocha <hbto@vauxoo.com>
2017-10-31 06:03:04 -0400

    [FIX] Rewriting Constraints on Baremo Matrix as SQL Constraint is not trustworthy because it have NULL values

--- commission_calculation/models/baremo.py
+++ commission_calculation/models/baremo.py
@@ -45,12 +45,13 @@ class BaremoLine(models.Model):
     _name = 'baremo.line'
     _order = "number asc"
 
-    name = fields.Char('Due Days Description', required=True, help="Due days Description")
-    number = fields.Integer('Due Days', required=True, help="Days since Emission/Due Date")
-    disc_ids = fields.One2many('baremo.discount', 'disc_id',
-                               'Commission per Discount @ Due Days',
-                               help="Commission per Discount @ Due Days")
-    baremo_id = fields.Many2one('baremo.book', 'Parent')
+    name = fields.Char(
+        'Description', required=True, help="Due days Description")
+    number = fields.Integer(
+        'Due Days', required=True, help="Days since Emission/Due Date")
+    disc_ids = fields.One2many(
+        'baremo.discount', 'disc_id', 'Commission per Discount @ Due Days')
+    baremo_id = fields.Many2one('baremo.book')
 
 
 class BaremoMatrix(models.Model):
@@ -58,45 +59,57 @@ class BaremoMatrix(models.Model):
     _name = 'baremo.matrix'
 
     baremo_id = fields.Many2one(
-        'baremo.book', 'Baremo', required=True)
+        'baremo.book', required=True)
     user_id = fields.Many2one('res.users', 'Salesman')
     product_id = fields.Many2one(
-        'product.product', 'Product', required=True)
+        'product.product', required=True)
 
-    @api.constrains('user_id')
+    @api.one
+    @api.constrains('user_id', 'baremo_id', 'product_id')
     def _check_salesman_empty(self):
-        """ A line with empty user is a general rule which
-        indicates that the rule of that product applies to all
-        salesman. This method avoids to create two o more general
-        rules apply to a same product in all baremo matrixes.
+        """ A line with empty user is a general rule which indicates that the
+        rule of that product applies to all salesman. This method avoids to
+        create two o more general rules apply to a same product in all baremo
+        matrixes.
+        As stated by Postgresql Documentation: https://goo.gl/jpiVir
+        In general, a unique constraint is violated if there is more than one
+        row in the table where the values of all of the columns included in the
+        constraint are equal. However, `two null values are never considered
+        equal in this comparison`. That means even in the presence of a unique
+        constraint it is possible to `store duplicate rows` that contain a null
+        value in at least one of the constrained columns. This behavior
+        conforms to the SQL standard, but we have heard that other SQL
+        databases might not follow this rule. So be careful when developing
+        applications that are intended to be portable.
         """
-        for line in self:
-            if not line.user_id and self.search(
-                [('product_id', '!=', self.product_id.id),
-                 ('user_id', '=', False)]):
-                raise ValidationError(
-                    _('A line of %s already exists for all salesman.')
-                    % line.product_id.name)
-
-    # Just this constraints avoid create two o more rules apply to
-    # a same product and user and not take into account when the user
-    # is empty.
-    _sql_constraints = [
-        ('baremo_permutation_unique',
-         'unique(user_id, product_id)',
-         'Same Salesman & Product can be assigned to only one Baremo')]
+        if len(self.search([
+                ('product_id', '=', self.product_id.id),
+                ('user_id', '=', self.user_id.id)])) == 1:
+            return True
+        msg = _('There are already baremos with the following settings: \n')
+        for l in self.search([
+                ('product_id', '=', self.product_id.id),
+                ('user_id', '=', self.user_id.id)]) - self:
+            msg += 'Product: %s, Baremo: %s, Salesman: %s\n' % (
+                l.product_id.name, l.baremo_id.name,
+                l.user_id.name or _('All Salesmen'))
+        msg += _('While you are creating this one:\n')
+        for l in self:
+            msg += 'Product: %s, Baremo: %s, Salesman: %s\n' % (
+                l.product_id.name, l.baremo_id.name,
+                l.user_id.name or _('All Salesmen'))
+        raise ValidationError(msg)
 
 
 class BaremoDiscount(models.Model):
 
     _name = 'baremo.discount'
     _order = "porc_disc asc"
-    _rec_name = 'porc_disc'
 
-    porc_disc = fields.Float('% Disc.', digits=dp.get_precision('Commission'),
-                             required=True,
-                             help="Percent discount per product")
-    porc_com = fields.Float('% Comm.', digits=dp.get_precision('Commission'),
-                            required=True,
-                            help="Percent commission @ Percent discount")
+    porc_disc = fields.Float(
+        '% Disc.', digits=dp.get_precision('Commission'),
+        required=True, help="Percent discount per product")
+    porc_com = fields.Float(
+        '% Comm.', digits=dp.get_precision('Commission'),
+        required=True, help="Percent commission @ Percent discount")
     disc_id = fields.Many2one('baremo.line', 'Baremo')
