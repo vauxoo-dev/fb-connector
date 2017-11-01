6cc92e2f7bb125405c5c5fdc35749877addcc42d
Hbto [ Vauxoo ] <hbto@users.noreply.github.com>
2017-11-01 13:39:47 -0400

    [FIX] Avoid to use a backslash char inside string

--- commission_calculation/models/commission_template.py
+++ commission_calculation/models/commission_template.py
@@ -18,6 +18,6 @@ class CommissionTemplate(models.Model):
     _inherit = 'commission.abstract'
 
     name = fields.Char(
-        'Template\'s name', required=True,
+        "Template's name", required=True,
         track_visibility='onchange',
         help="Commission template's name")
